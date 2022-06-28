966ec5abf90ce1c982ed90e88fb0b24b826ff1fb
GustavoPeters <107434668+GustavoPeters@users.noreply.github.com>
2022-06-28 16:20:16 -0300

    Add files via upload

---
+++ EncontroRemoto2.js
@@ -0,0 +1,20 @@
+let numeroDeAlunos = 100
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    console.log(contador);
+    
+
+     if (contador == 0)  {
+        console.log ("Numero igual a zero")
+    
+     }
+     else if (contador % 2 == 0) {
+            console.log("Número Par")
+     }
+     else {
+            console.log ("Número Impar")
+     }
+
+     
+}
+    
